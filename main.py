d471896eed68cdd9361431c951fc05d38c233b92
Siem <vankeulensiem@gmail.com>
2025-06-17 14:55:36 +0200

    os.system("git init")

--- main.py
+++ main.py
@@ -15,14 +15,18 @@ def main(
         if type == "js":
             os.system("mkdir " + name)
             os.system("cd " + name + " && npm init -y")
+            os.system("git init")
         elif type == "python":
             os.system("mkdir " + name)
             os.system("cd " + name + " && python -m venv venv")
+            os.system("git init")
         elif type == "cf":
             os.system("mkdir " + name)
             os.system("cd " + name)
             os.system("git init")
-            os.system("git submodule add ")
+            os.system(
+                "git submodule add https://github.com/supersiem/custom_framework.git"
+            )
     elif command == "clone":
         if not url:
             typer.echo("Error: --url is required for 'clone' command.")
